8ee8db0814519f2e96b2c7573c7e6e9bc461f803
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-28 07:38:16 -0700

    update client to debug response from http request to get all airports

--- backend/client/client.go
+++ backend/client/client.go
@@ -4,6 +4,7 @@ import (
    "encoding/json"
    "log"
    "net/http"
+   "io/ioutil"
 
    "github.com/leonardyeoxl/Thales-AIR-Lab-Tech-Challenge/backend/model"
 )
@@ -14,22 +15,31 @@ func FetchAirports() (string, error) {
    URL := "https://open-atms.airlab.aero/api/v1/airac/airports"
 	// Make HTTP request using the Get function
 	req, err := http.NewRequest("GET", URL, nil)
-    if err != nil {
-        log.Fatal(err)
-    }
-    req.Header.Set("Content-Type", contentType)
-    resp, err := http.DefaultClient.Do(req)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer resp.Body.Close()
-
-	// Create a variable of the same type as our model
-   var airportResp model.AirportResponse
-	// Decode the data
-   if err := json.NewDecoder(resp.Body).Decode(&airportResp); err != nil {
+   if err != nil {
       log.Fatal(err)
    }
-	// Invoke the text output function & return it with nil as the error value
-   return airportResp.TextOutput(), nil
+   req.Header.Set("Content-Type", contentType)
+   resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      log.Fatal(err)
+   }
+   defer resp.Body.Close()
+
+   bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(string(bytes))
+
+   return string(bytes), nil
+
+	// // Create a variable of the same type as our model
+   // var airportResp model.AirportResponse
+	// // Decode the data
+   // if err := json.NewDecoder(resp.Body).Decode(&airportResp); err != nil {
+   //    log.Fatal(err)
+   // }
+	// // Invoke the text output function & return it with nil as the error value
+   // return airportResp.TextOutput(), nil
 }
