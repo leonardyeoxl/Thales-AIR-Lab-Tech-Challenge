9d642c2575dc3b1d94f5e63cc98ad1b10163948a
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-28 07:26:36 -0700

    update dockerfile and model

--- backend/model/model.go
+++ backend/model/model.go
@@ -6,17 +6,19 @@ import (
 
 // Cryptoresponse is exported, it models the data we receive.
 type AirportResponse []struct {
-   Name              string    `json:"name"`
-   Price             string    `json:"price"`
-   Rank              string    `json:"rank"`
-   High              string    `json:"high"`
-   CirculatingSupply string    `json:"circulating_supply"`
+   UID              string    `json:"uid"`
+   NAME             string    `json:"name"`
+   IATA             string    `json:"iata"`
+   ICAO             string    `json:"icao"`
+   LAT              float32    `json:"lat"`
+   LNG              float32    `json:"lng"`
+   ALT              float32    `json:"alt"`
 }
 
 //TextOutput is exported,it formats the data to plain text.
-func (c Cryptoresponse) TextOutput() string {
-p := fmt.Sprintf(
-  "Name: %s\nPrice : %s\nRank: %s\nHigh: %s\nCirculatingSupply: %s\n",
-  c[0].Name, c[0].Price, c[0].Rank, c[0].High, c[0].CirculatingSupply)
-   return p
+func (airport AirportResponse) TextOutput() string {
+   response_out := fmt.Sprintf(
+   "Name: %s\nPrice : %s\nRank: %s\nHigh: %s\nCirculatingSupply: %d\n",
+   airport[0].UID, airport[0].NAME, airport[0].IATA, airport[0].ICAO, airport[0].LAT, airport[0].LNG, airport[0].ALT)
+      return response_out
 }
