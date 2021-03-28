3646bf9a9a5224b542f600cabc31421307715e81
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-28 08:13:02 -0700

    fix backend main

--- backend/main.go
+++ backend/main.go
@@ -3,13 +3,12 @@ package main
 import (
     "encoding/json"
     "fmt"
-    "log"
 
     "github.com/leonardyeoxl/Thales-AIR-Lab-Tech-Challenge/backend/client"
 )
 
 func main() {
-    airports, err := client.FetchAirports()
+    airports := client.FetchAirports()
     newsJson, err := json.Marshal(airports)
     if err != nil {
         panic(err)
