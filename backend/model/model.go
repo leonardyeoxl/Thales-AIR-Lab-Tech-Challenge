8d66e144f88bbc58e3ca9ec4d52cc0787b492a53
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-28 08:06:59 -0700

    fix

--- backend/model/model.go
+++ backend/model/model.go
@@ -16,11 +16,3 @@ type AirportItem struct {
 }
 
 type Airport []AirportItem
-
-//TextOutput is exported,it formats the data to plain text.
-func (airport AirportItem) TextOutput() string {
-   response_out := fmt.Sprintf(
-   "UID: %s\nNAME : %s\nIATA: %s\nICAO: %s\nLAT: %f\nLNG: %f\nALT: %d\n",
-   airport[0].UID, airport[0].NAME, airport[0].IATA, airport[0].ICAO, airport[0].LAT, airport[0].LNG, airport[0].ALT)
-   return response_out
-}
