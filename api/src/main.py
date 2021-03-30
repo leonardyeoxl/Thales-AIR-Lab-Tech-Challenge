f64df38af85d57580cd8c68c0fadf4bd79b08e5c
leonardyeoxl <leonardyeoxl@gmail.com>
2021-03-30 09:18:17 -0700

    add support for frontend

--- api/src/main.py
+++ api/src/main.py
@@ -2,8 +2,19 @@ from RPCClient import STARsSIDsRpcClient
 import json
 from typing import Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
---
+++ frontend/frontend/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="hello">
+    <ul id="app">
+      <li v-for="item in airport_list" :key="item.SIDs_airport_list">
+        {{ item.SIDs_airport_list }}
+      </li>
+      <li v-for="item in airport_list" :key="item.STARs_airport_list">
+        {{ item.STARs_airport_list }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  el: '#app',
+  data() {
+    return {
+      airport_list: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://127.0.0.1:8000/STARS-AND-SIDS')
+      .then(response => (this.airport_list = response))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
